44afa8b84dbe52bf237a70d17628e5fc8fcf5cbf
ArmanGitHubg <54878103+ArmanGitHubg@users.noreply.github.com>
2021-01-20 01:43:07 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import logo from './logo.svg';
+import './App.css';
+import Practice from "./Practice"
+
+function App() {
+  return (
+    <div>
+      hello react with Now with arman
+      <Practice />
+    </div>
+  )
+}
+
+export default App;
